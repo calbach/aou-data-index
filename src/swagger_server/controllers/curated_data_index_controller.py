5fe634ab59ef3e42447e2e922ae5561fd763e34a
CH Albach <calbach@google.com>
2017-06-13 16:45:26 -0700

    Implement dataset methods over ElasticSearch

--- src/swagger_server/controllers/curated_data_index_controller.py
+++ src/swagger_server/controllers/curated_data_index_controller.py
@@ -1,4 +1,9 @@
+import base64
 import connexion
+import json
+import urllib
+import requests
+from werkzeug.exceptions import BadRequest
 from swagger_server.models.cdr_data_pointer import CdrDataPointer
 from swagger_server.models.cdr_dataset import CdrDataset
 from swagger_server.models.cdr_individual import CdrIndividual
@@ -7,6 +12,48 @@ from datetime import date, datetime
 from typing import List, Dict
 from six import iteritems
 from ..util import deserialize_date, deserialize_datetime
+
+
+# TODO(calbach): Figure out how to plumb parameters through, not globals.
+INDEX_ADDR = "http://localhost:9200"
+ES_HEADERS = {'content-type': 'application/json'}
+
+def ds_index_type():
+  return INDEX_ADDR + '/cdr/datasets'
+
+def ds_index_path(ds_id):
+  return ds_index_type() + '/' + ds_id
+
+def by_ppl_index(ds_id):
+  return INDEX_ADDR + '/by-individual:' + ds_id
+
+def by_data_index(ds_id):
+  return INDEX_ADDR + '/by-data:' + ds_id
+
+def ppl_by_ppl_index_path(ds_id, id):
+  return by_ppl_index(ds_id) + '/individuals/' + id
+
+def data_by_ppl_index_path(ds_id, uri, ind_id):
+  return by_ppl_index(ds_id) + '/data/{}?parent={}'.format(urllib.urlencode(uri), ind_id)
+
+def data_by_data_index_path(ds_id, uri):
+  return by_data_index(ds_id) + '/data/' + urllib.urlencode(uri)
+
+def ppl_by_data_index_path(ds_id, indID, uri):
+  return by_data_index(ds_id) + '/individuals/{}?parent={}'.format(indID, urllib.urlencode(uri))
+
+def split_ds_name(ds_name):
+  parts = ds_name.split('/')
+  if len(parts) != 2 or parts[0] != 'datasets':
+    raise BadRequest()
+  return parts[1]
+
+
+def doc_to_dataset(doc):
+  return CdrDataset(name='datasets/' + doc['_id'])
+
+def dataset_to_doc(dataset):
+  return {}
 
 
 def create_data_pointer(dataset_id, body):
@@ -23,6 +70,8 @@ def create_data_pointer(dataset_id, body):
     if not connexion.request.is_json:
       return {}, 400
     dp = CdrDataPointer.from_dict(connexion.request.get_json())
+    if not dp or not dp.Uri:
+      return {}, 400
     # TODO(calbach): Call ES.
     return dp.to_dict()
 
@@ -40,7 +89,32 @@ def create_dataset(body):
     if not connexion.request.is_json:
       return {}, 400
     dataset = CdrDataset.from_dict(connexion.request.get_json())
-    # TODO(calbach): Call ES.
+    if not dataset or not dataset.name:
+      return {}, 400
+
+    ds_id = split_ds_name(dataset.name)
+    requests.put(ds_index_path(ds_id), json={},
+                 headers=ES_HEADERS).raise_for_status()
+    requests.put(by_ppl_index(ds_id), json={
+        "mappings": {
+            "individuals": {},
+            "data": {
+                "_parent": {
+                    "type": "individuals",
+                },
+            },
+        },
+    }, headers=ES_HEADERS).raise_for_status()
+    requests.put(by_data_index(ds_id), json={
+        "mappings": {
+            "data": {},
+            "individuals": {
+                "_parent": {
+                    "type": "data",
+                },
+            },
+        },
+    }, headers=ES_HEADERS).raise_for_status()
     return dataset.to_dict()
 
 
@@ -81,7 +155,9 @@ def delete_dataset(dataset_id):
     Args:
       dataset_id: the dataset to delete
     """
-    # TODO(calbach): Call ES.
+    requests.delete(ds_index_path(dataset_id)).raise_for_status()
+    requests.delete(by_ppl_index(dataset_id)).raise_for_status()
+    requests.delete(by_data_index(dataset_id)).raise_for_status()
     pass
 
 
@@ -122,8 +198,9 @@ def get_dataset(dataset_id):
     Returns:
       a dictionary representation of a CdrDataset
     """
-    # TODO(calbach): Call ES.
-    return {}
+    r = requests.get(ds_index_path(dataset_id))
+    r.raise_for_status()
+    return doc_to_dataset(r.json())
 
 
 def get_individual(dataset_id, individual_id):
@@ -141,7 +218,27 @@ def get_individual(dataset_id, individual_id):
     return {}
 
 
-def list_datasets(page_size=None, page_token=None):
+def encode_ds_page_token(offset):
+  """Encode the dataset pagination token."""
+  # We implement the pagination token via base64-encoded JSON s.t. tokens are
+  # opaque to clients and enable us to make backwards compatible changes to our
+  # pagination implementation. Base64+JSON are used specifically as they are
+  # language-independent standards.
+  s = json.dumps({
+      'offset': offset,
+  })
+  # Strip ugly base64 padding.
+  return base64.urlsafe_b64encode(s).rstrip('=')
+
+
+def decode_ds_page_token(token):
+  """Decode the dataset pagination token."""
+  padded_token = token + '=' * (len(token)%4)
+  tok = base64.urlsafe_b64decode(padded_token)
+  return json.loads(tok)
+
+
+def list_datasets(page_size=32, page_token=None):
     """
     Lists datasets.
 
@@ -152,8 +249,34 @@ def list_datasets(page_size=None, page_token=None):
     Returns:
       a dictionary representation of a CdrListDatasetsResponse
     """
-    # TODO(calbach): Call ES.
-    return {}
+    es_req = {
+        'size': page_size + 1,
+        'query': {
+            'match_all': {},
+        },
+        'sort': [
+            '_doc', # Index order.
+        ],
+    }
+    offset = 0
+    if page_token:
+      tok = decode_ds_page_token(page_token)
+      offset = tok.get('offset')
+      es_req['from'] = offset
+
+    r = requests.post(ds_index_type() + '/_search',
+                      json=es_req, headers=ES_HEADERS)
+    r.raise_for_status()
+
+    next_page_token = None
+    hits = r.json().get('hits').get('hits')
+    if len(hits) > page_size:
+      hits = hits[:-1]
+      next_page_token = encode_ds_page_token(offset+page_size)
+    return CdrListDatasetsResponse(
+        datasets=[doc_to_dataset(d) for d in hits],
+        next_page_token=next_page_token,
+    ).to_dict()
 
 
 def update_data_pointer(dataset_id, data_pointer_id, body):
@@ -189,7 +312,15 @@ def update_dataset(dataset_id, body):
     if not connexion.request.is_json:
       return {}, 400
     dataset = CdrDataset.from_dict(connexion.request.get_json())
-    # TODO(calbach): Call ES.
+    if dataset.name:
+      ds_id = split_ds_name(dataset.name)
+      if ds_id != dataset_id:
+        raise BadRequest()
+
+    r = requests.post(ds_index_path(dataset_id) + '/_update', json={
+        'doc': dataset_to_doc(dataset),
+    }, headers=ES_HEADERS)
+    r.raise_for_status()
     return dataset.to_dict()
 
 
