a51d9c733edbee77706d17d95fa5d2e46e6f0d18
CH Albach <calbach@google.com>
2017-08-02 09:54:32 -0700

    creates

--- data_index/controllers/data_pointers_controller.py
+++ data_index/controllers/data_pointers_controller.py
@@ -3,7 +3,7 @@ from data_index.models.data_pointer import DataPointer
 from datetime import date, datetime
 from typing import List, Dict
 from six import iteritems
-from werkzeug.exceptions import NotImplemented
+from werkzeug.exceptions import BadRequest, InternalServerError, NotImplemented
 from ..util import deserialize_date, deserialize_datetime
 
 
@@ -18,9 +18,41 @@ def create_data_pointer(datasetId, body):
     Returns:
       a dictionary representing a DataPointer
     """
-    if connexion.request.is_json:
-        body = DataPointer.from_dict(connexion.request.get_json())
-    raise NotImplemented()
+    if not connexion.request.is_json:
+        raise InternalServerError('got unexpected non-JSON payload')
+    dp = DataPointer.from_dict(body)
+    if not dp or not dp.uri:
+        raise BadRequest('missing required dataPointer.uri from request')
+
+    # Will raise NotFound if the dataset doesn't exist.
+    get_dataset(datasetId)
+
+    dp.name, dp_id = data_pointer_name(datasetId, dp.uri)
+    individuals = dict()
+    for name in (dp.individual_names or []):
+        ind_ds_id, ind_id = split_individual_name(name)
+        if datasetId != ind_ds_id:
+            raise BadRequest('dataPointer.individualNames must belong to the same dataset as the dataPointer ("datasets/{}"), got "{}"'.format(datasetId, name))
+        # Will raise NotFound if individual doesn't exist.
+        # TODO(calbach): Could batch these requests.
+        individuals[ind_id] = get_individual(datasetId, ind_id)
+
+    # Add the data pointer documents to the data indices.
+    dp_paths = [data_by_data_index_path(datasetId, dp_id)]
+    for ind_id in individuals:
+        dp_paths.append(data_by_ppl_index_path(datasetId, dp_id, ind_id))
+
+    dp_doc = data_pointer_to_doc(dp)
+    for path in dp_paths:
+        requests.put(path, json=dp_doc, headers=elastic.REQ_HEADERS).raise_for_status()
+
+    # Add the associated individuals documents to the individuals (by data) index.
+    for (ind_id, ind) in individuals.iteritems():
+        requests.put(ppl_by_data_index_path(datasetId, ind_id, dp_id),
+                     json=individual_to_doc(ind),
+                     headers=elastic.REQ_HEADERS).raise_for_status()
+
+    return dp
 
 
 def get_data_pointer(datasetId, dataPointerId):
--- data_index/controllers/individuals_controller.py
+++ data_index/controllers/individuals_controller.py
@@ -1,10 +1,18 @@
 import connexion
 from data_index.models.individual import Individual
+from data_index import elastic
 from datetime import date, datetime
 from typing import List, Dict
 from six import iteritems
-from werkzeug.exceptions import NotImplemented
+from werkzeug.exceptions import BadRequest, InternalServerError, NotImplemented
 from ..util import deserialize_date, deserialize_datetime
+
+
+def split_individual_name(name):
+  parts = name.split('/')
+  if len(parts) != 4 or parts[0] != 'datasets' or parts[2] != 'individuals':
+    raise BadRequest('malformed individual name "{}"'.format(name))
+  return (parts[1], parts[3])
 
 
 def create_individual(datasetId, body):
@@ -17,9 +25,25 @@ def create_individual(datasetId, body):
     Returns:
       a dictionary representing a Individual
     """
-    if connexion.request.is_json:
-        body = Individual.from_dict(connexion.request.get_json())
-    raise NotImplemented()
+    if not connexion.request.is_json:
+        raise InternalServerError('got unexpected non-JSON payload')
+    individual = Individual.from_dict(body)
+    if not individual or not individual.name:
+        raise BadRequest('missing required individual.name from request')
+
+    (ds_id, ind_id) = split_individual_name(individual.name)
+    if ds_id != datasetId:
+        raise BadRequest('dataset name "datasets/{}" from URL must agree with ' +
+                         'individual.name "{}"'.format(datasetId, individual.name))
+
+    # Will raise NotFound if the dataset doesn't exist.
+    get_dataset(datasetId)
+
+    requests.put(
+        ppl_by_ppl_index_path(ds_id, ind_id),
+        json=individual_to_doc(individual),
+        headers=elastic.REQ_HEADERS).raise_for_status()
+    return individual
 
 
 def get_individual(datasetId, individualId):
